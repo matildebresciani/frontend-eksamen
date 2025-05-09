350819fd8646f2fbecd42806f1aa5984868a0b43
Katinka H. Bjerke <kabj0002@stud.kea.dk>
2025-05-09 11:11:19 +0200

    update header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,17 +6,22 @@ import Button from "./Button";
 import Link from "next/link";
 
 const Header = () => {
-    return ( <header className="flex items-center justify-between py-10 px-6">
-      <div>Logo</div>
+  return (
+    <header className="flex items-center justify-between py-10 px-6">
+      <Link href="/" className="flex">
+        <div>Logo</div>
+        <h1>MODERNIA CURATORS</h1>
+      </Link>
       <HeaderNav />
-    </header> );
-}
+    </header>
+  );
+};
 
 const HeaderNav = () => {
-    const { openSignIn } = useClerk();
-    return ( 
-        <nav className="flex items-center gap-8">
-        <Link href="/events">SE EVENTS</Link>
+  const { openSignIn } = useClerk();
+  return (
+    <nav className="flex items-center gap-8">
+      <Link href="/events">SE EVENTS</Link>
       <SignedOut>
         <Button variant="primary" onClick={() => openSignIn()}>
           LOG IND
@@ -24,14 +29,16 @@ const HeaderNav = () => {
       </SignedOut>
       <SignedIn>
         <div className="flex items-center gap-4">
-        <Link href="/create-event">
-          <Button variant="primary">OPRET EVENT</Button>
-        </Link>
-        <UserButton/>
+          <Link href="/create-event">
+            <Button variant="primary" className="">
+              OPRET EVENT
+            </Button>
+          </Link>
+          <UserButton />
         </div>
       </SignedIn>
-      </nav>
-     );
-}
- 
+    </nav>
+  );
+};
+
 export default Header;
