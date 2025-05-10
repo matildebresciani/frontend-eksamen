4115823cc1670f46b89710c2e8e037d6f9028b0d
matilde <mabr0005@stud.kea.dk>
2025-05-10 15:13:45 +0200

    zustand city store

--- src/app/events/page.jsx
+++ src/app/events/page.jsx
@@ -1,5 +1,43 @@
-// Matilde
+//Matilde
+
+"use client";
+
+import { useEffect, useState } from "react";
+import EventCard from "@/components/events/EventCard";
+import SelectCity from "@/components/events/SelectCity";
+import useCityStore from "../store/cityStore";
 
 export default function Page() {
-    return <div>Events</div>;
-  }
+  const [events, setEvents] = useState([]);
+  const { selectedCities, normalizeCity } = useCityStore();
+
+  useEffect(() => {
+    fetch("http://localhost:8080/events")
+      .then((res) => res.json())
+      .then((data) => setEvents(data));
+  }, []);
+
+  const filteredEvents = selectedCities.includes("Alle Byer") || selectedCities.length === 0
+    ? events
+    : events.filter((event) =>
+        selectedCities.includes(normalizeCity(event.location.address))
+      );
+
+  return (
+    <section>
+      <h1>Events</h1>
+      <p>
+        Her finder du en oversigt over spændende kunstevents i hele Danmark. Glæd dig til alt fra Monets klassiske mesterværker til nyskabende moderne kunst og installationer. Udstillingerne spænder bredt – fra intime gallerier til store museer. Der er noget for enhver smag, uanset om du er kunstelsker eller nysgerrig nybegynder. Vi glæder os til at se dig!
+      </p>
+      <div className="border-2 border-black w-full my-4"></div>
+      <div className="grid grid-cols-[1fr_2fr] gap-4">
+        <SelectCity />
+        <div className="flex flex-col gap-4">
+          {filteredEvents.map((event) => (
+            <EventCard key={event.id} event={event} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/components/events/EventCard.jsx
+++ src/components/events/EventCard.jsx
@@ -1,1 +1,28 @@
 //Matilde
+import { LuMapPin } from "react-icons/lu";
+import Image from "next/image";
+
+const EventCard = ({event}) => {
+    return ( <div className="flex gap-4 my-8 pb-8 max-w-[818px] border-b-2 border-black">
+        <figure className="bg-primary-red w-[159px] h-[174px] shrink-0">
+        </figure>
+        <div>
+        <h3 className="uppercase">{new Date(event.date).toLocaleDateString("da-DK", {
+            weekday: "long",
+            day: "numeric",
+            month: "long"
+            })}</h3>
+            <p className="italic">"{event.title}"</p>
+            <p className="font-bold">Kl. 10.00</p>
+            <div className="flex flex-row items-center gap-1">
+                <LuMapPin className="text-primary-red" />
+                <p>{event.location.name}, {event.location.address}</p>
+            </div>
+            <p className="line-clamp-3">
+            {event.description}
+            </p>
+        </div>
+    </div> );
+}
+ 
+export default EventCard;
