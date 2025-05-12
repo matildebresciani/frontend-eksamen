c707e567af0294a514ecddcee7464e92d64a9811
matilde <mabr0005@stud.kea.dk>
2025-05-12 09:43:21 +0200

    event card hover og link

--- src/components/BtnWithArrow.jsx
+++ src/components/BtnWithArrow.jsx
@@ -1,2 +1,15 @@
 //bruges til "se event" (på event card) og "læs mere" (på art card)
 //animation på pil ved hover
+import Link from "next/link";
+import { FaArrowRight } from "react-icons/fa6";
+
+const BtnWithArrow = ({href, children}) => {
+    return ( 
+        <div className="flex justify-between items-center gap-2">
+            {children}
+            <FaArrowRight size={18} />
+        </div>
+     );
+}
+ 
+export default BtnWithArrow;
--- src/components/events/EventCard.jsx
+++ src/components/events/EventCard.jsx
@@ -1,18 +1,28 @@
 //Matilde
 import { LuMapPin } from "react-icons/lu";
 import Image from "next/image";
+import Link from "next/link";
+import BtnWithArrow from "../BtnWithArrow";
 
 const EventCard = ({event}) => {
-    return ( <div className="flex gap-4 my-8 pb-8 max-w-[818px] border-b-2 border-black">
+    
+    return ( 
+        <Link href={`/events/${event.id}`}>
+    <div className=" group flex gap-4 my-8 pb-8 max-w-[818px] border-b-2 border-black">
         <figure className="bg-primary-red w-[159px] h-[174px] shrink-0">
         </figure>
         <div>
-        <h3 className="uppercase">{new Date(event.date).toLocaleDateString("da-DK", {
+        <div className="flex justify-between">
+            <h3 className="uppercase !text-primary-red">{new Date(event.date).toLocaleDateString("da-DK", {
             weekday: "long",
             day: "numeric",
             month: "long"
             })}</h3>
-            <p className="italic">"{event.title}"</p>
+            <div className="opacity-0 group-hover:opacity-100 transition-opacity duration-200 ease-in-out">
+            <BtnWithArrow>SE EVENT</BtnWithArrow>
+            </div>
+        </div>
+            <p className="italic font-medium">"{event.title}"</p>
             <p className="font-bold">Kl. 10.00</p>
             <div className="flex flex-row items-center gap-1">
                 <LuMapPin className="text-primary-red" />
@@ -22,7 +32,8 @@ const EventCard = ({event}) => {
             {event.description}
             </p>
         </div>
-    </div> );
+    </div>
+    </Link> );
 }
  
 export default EventCard;
