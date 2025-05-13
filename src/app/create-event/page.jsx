bf2648d441ff1d9587e332e3f6ae6baa1509c15a
matilde <mabr0005@stud.kea.dk>
2025-05-13 12:21:53 +0200

    line

--- src/app/create-event/page.jsx
+++ src/app/create-event/page.jsx
@@ -6,7 +6,7 @@ import EventForm from "@/components/edit_create_event/InputForm";
 export default function Page() {
   return <div>
     <h1>Opret Events</h1>
-    <div className="grid grid-cols-[1fr_2fr]">
+    <div className="grid grid-cols-[1fr_5px_2fr] gap-4">
         <EventForm></EventForm>
         <span className="border-l-2 border-black"></span>
         <ArtworkList></ArtworkList>
