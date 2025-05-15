4b8afaaeb58d77a8f9c4b4d8de96ebfb42d1d4e7
matilde <mabr0005@stud.kea.dk>
2025-05-15 17:24:30 +0200

    create event grid

--- src/app/create-event/page.jsx
+++ src/app/create-event/page.jsx
@@ -1,15 +1,14 @@
 //Matilde og Katinka
 
 import ArtworkList from "@/components/edit_create_event/ArtworkList";
-//import EventForm from "@/components/edit_create_event/EventForm";
+import EventForm from "@/components/edit_create_event/InputForm";
 
 export default function Page() {
   return (
-    <div className="grid sm:grid-cols-2">
+    <div>
       <h1>Opret Events</h1>
-      <div>
-        {/* <div className="grid grid-cols-[1fr_5px_2fr] gap-4"> */}
-        {/* <EventForm></EventForm> */}
+      <div className="grid grid-cols-[1fr_5px_2fr] gap-4">
+        <EventForm></EventForm>
         <span className="border-l-2 border-black"></span>
         <ArtworkList />
       </div>
