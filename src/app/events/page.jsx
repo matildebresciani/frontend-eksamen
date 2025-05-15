1f0f24588c05a5c9fd7b2638c6ec25c1b654e8f1
Katinka H. Bjerke <kabj0002@stud.kea.dk>
2025-05-15 15:47:32 +0200

    update event cards

--- src/app/events/page.jsx
+++ src/app/events/page.jsx
@@ -17,20 +17,26 @@ export default function Page() {
       .then((data) => setEvents(data));
   }, []);
 
-  const filteredEvents = selectedCities.includes("Alle Byer") || selectedCities.length === 0
-    ? events
-    : events.filter((event) =>
-        selectedCities.includes(normalizeCity(event.location.address))
-      );
+  const filteredEvents =
+    selectedCities.includes("Alle Byer") || selectedCities.length === 0
+      ? events
+      : events.filter((event) =>
+          selectedCities.includes(normalizeCity(event.location.address))
+        );
 
   return (
     <section>
       <h1>Events</h1>
       <p>
-        Her finder du en oversigt over spændende kunstevents i hele Danmark. Glæd dig til alt fra Monets klassiske mesterværker til nyskabende moderne kunst og installationer. Udstillingerne spænder bredt – fra intime gallerier til store museer. Der er noget for enhver smag, uanset om du er kunstelsker eller nysgerrig nybegynder. Vi glæder os til at se dig!
+        Her finder du en oversigt over spændende kunstevents i hele Danmark.
+        Glæd dig til alt fra Monets klassiske mesterværker til nyskabende
+        moderne kunst og installationer. Udstillingerne spænder bredt – fra
+        intime gallerier til store museer. Der er noget for enhver smag, uanset
+        om du er kunstelsker eller nysgerrig nybegynder. Vi glæder os til at se
+        dig!
       </p>
       <div className="border-2 border-black w-full my-4"></div>
-      <div className="grid grid-cols-[1fr_2fr] gap-4">
+      <div className="grid sm:grid-cols-[1fr_2fr] gap-4">
         <SelectCity />
         <div className="flex flex-col gap-4">
           {filteredEvents.map((event) => (
--- src/components/events/EventCard.jsx
+++ src/components/events/EventCard.jsx
@@ -8,47 +8,46 @@ import { useEffect } from "react";
 import { useClerk, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import EditIcon from "../edit_create_event/EditIcon";
 
-const EventCard = ({event}) => {
-    const setDate = useEventStore((state) => state.setDate);
-    const formattedDate = useEventStore((state) => state.formattedDate);
+const EventCard = ({ event }) => {
+  const setDate = useEventStore((state) => state.setDate);
+  const formattedDate = useEventStore((state) => state.formattedDate);
 
-    useEffect(() => {
-        setDate(event.date);
-      }, [event.date]);
-    
-    return ( 
-        <Link href={`/events/${event.id}`}>
-    <div className="group/card flex gap-4 pb-4 my-4 max-w-[818px] border-b-2 border-black">
-        <figure className="bg-primary-red w-[159px] h-[174px] shrink-0">
-        </figure>
+  useEffect(() => {
+    setDate(event.date);
+  }, [event.date]);
+
+  return (
+    <Link href={`/events/${event.id}`}>
+      <div className="group/card flex flex-row-reverse sm:flex-row  gap-4 pb-4 my-4 max-w-[818px] border-b-2 border-black">
+        <figure className="bg-primary-red w-[119px] h-[134px] sm:w-[159px] sm:h-[174px] shrink-0"></figure>
         <div>
-        <div className="flex justify-between">
-            <h3 className="uppercase !text-primary-red">{formattedDate}</h3>
-            <div className="opacity-0 group-hover/card:opacity-100 transition-opacity duration-200 ease-in-out">
-            <BtnWithArrow>SE EVENT</BtnWithArrow>
+          <div className="flex justify-between span">
+            <h3 className="uppercase !text-primary-red ">{formattedDate}</h3>
+            <div className=" opacity-0 group-hover/card:opacity-100 transition-opacity duration-200 ease-in-out">
+              <BtnWithArrow>SE EVENT</BtnWithArrow>
             </div>
-        </div>
-      
-            <p className="italic font-medium">"{event.title}"</p>
-            <p className="font-bold">Kl. 10.00</p>
-            <div className="flex flex-row items-center gap-1">
-                <LuMapPin className="text-primary-red" />
-                <p>{event.location.name}, {event.location.address}</p>
-            </div>
-            <div className="flex items-end gap-4">
-            <p className="line-clamp-3">
-            {event.description}
-            </p>
-            <div className="flex justify-end">
-            <SignedIn>
-                <EditIcon />
-            </SignedIn>
-        </div>
+          </div>
 
+          <p className="italic font-medium">"{event.title}"</p>
+          <p className="font-bold">Kl. 10.00</p>
+          <div className="flex flex-row items-center gap-1">
+            <LuMapPin className="text-primary-red" />
+            <p>
+              {event.location.name}, {event.location.address}
+            </p>
+          </div>
+          <div className="flex items-end gap-4">
+            <p className="line-clamp-3">{event.description}</p>
+            <div className="flex justify-end">
+              <SignedIn>
+                <EditIcon />
+              </SignedIn>
             </div>
+          </div>
         </div>
-    </div>
-    </Link> );
-}
- 
+      </div>
+    </Link>
+  );
+};
+
 export default EventCard;
