569e279bc2b5e9929d63e5f54aa666eb43d5a5ed
Katinka H. Bjerke <kabj0002@stud.kea.dk>
2025-05-16 13:11:10 +0200

    update buttons

--- src/components/edit_create_event/EditIcon.jsx
+++ src/components/edit_create_event/EditIcon.jsx
@@ -1,10 +1,11 @@
 import { FiEdit3 } from "react-icons/fi";
 
 const EditIcon = () => {
-    return ( <button
-        className="bg-primary-red hover:bg-[#F04F4F] text-white rounded-full p-2 ease-in-out duration-200 w-10 h-10">
-        <FiEdit3 size={24}></FiEdit3>
-    </button> );
-}
- 
+  return (
+    <button className="bg-primary-red hover:bg-primary-red-hover2 text-white rounded-full p-2 ease-in-out duration-200 w-10 h-10">
+      <FiEdit3 size={24}></FiEdit3>
+    </button>
+  );
+};
+
 export default EditIcon;
