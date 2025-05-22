ee6fd3220ed60b06f45b7e61dd07d4f891005ea7
matilde <mabr0005@stud.kea.dk>
2025-05-22 12:30:34 +0200

    start til sammenkobling af form og artworks

--- src/app/create-event/page.jsx
+++ src/app/create-event/page.jsx
@@ -4,13 +4,18 @@ import ArtworkList from "@/components/edit_create_event/ArtworkList";
 import EventForm from "@/components/edit_create_event/forms/InputForm";
 
 export default function Page() {
+    const [selectedArtworks, setSelectedArtworks] = useState([]);
+
+
   return (
     <div>
       <h1>Opret Events</h1>
       <div className="grid grid-cols-[1fr_5px_2fr] gap-4">
-        <EventForm></EventForm>
+        <EventForm selectedArtworks={selectedArtworks}></EventForm>
         <span className="border-l-2 border-black"></span>
-        <ArtworkList />
+        <ArtworkList
+        selectedArtworks={selectedArtworks}
+          setSelectedArtworks={setSelectedArtworks} />
       </div>
     </div>
   );
