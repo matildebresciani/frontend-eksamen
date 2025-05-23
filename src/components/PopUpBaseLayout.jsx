57b41648b39f53c4971d775e507e18afe72fcc0c
matilde <mabr0005@stud.kea.dk>
2025-05-23 13:53:44 +0200

    artwork gallery sat ind på rediger event

--- src/components/PopUpBaseLayout.jsx
+++ src/components/PopUpBaseLayout.jsx
@@ -1,7 +1,7 @@
 const PopUpBase = ({children}) => {
     return (         
         <div className="fixed inset-0 bg-black/50 backdrop-blur-sm flex items-center justify-center z-400">
-          <div className="bg-white rounded-lg p-6 shadow-lg min-w-sm max-w-fit w-full">
+          <div className="bg-white rounded-lg p-6 shadow-lg min-w-[320px] max-w-[95vw] w-full max-h-[90vh] overflow-auto">
             {children}
           </div>
         </div>
