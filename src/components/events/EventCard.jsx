ed4dd0e5a642ba52430d085fd9647b60ce714321
MajaOlsen <majabacholsen@gmail.com>
2025-05-26 21:51:57 +0200

    billede fetch på events liste

--- src/components/events/EventCard.jsx
+++ src/components/events/EventCard.jsx
@@ -7,21 +7,38 @@ import EditIcon from "../edit_create_event/EditIcon";
 import { formatDate } from "@/utils/formatDate";
 import DeleteBtn from "../edit_create_event/Delete";
 import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import { fetchArtworkById } from "@/api-mappe/SmkApiKald";
 
 const EventCard = ({ event, onDeleted, onEdit }) => {
   const router = useRouter();
   const formattedDate = formatDate(event.date);
 
+  const [artworkImg, setArtworkImg] = useState(null);
+
+  useEffect(() => {
+    const getArtworkImg = async () => {
+      if (event?.artworkIds && event.artworkIds.length > 0) {
+        try {
+          const data = await fetchArtworkById(event.artworkIds[0]);
+          const imgUrl = data?.items?.[0]?.image_thumbnail || null;
+          setArtworkImg(imgUrl);
+        } catch {
+          setArtworkImg(null);
+        }
+      } else {
+        setArtworkImg(null);
+      }
+    };
+    getArtworkImg();
+  }, [event]);
+
   const handleCardClick = () => {
     router.push(`/events/${event.id}`);
   };
 
-
   return (
-    <div
-      onClick={handleCardClick}
-      className="group/card container max-w-[818px] border-b-2 border-black pb-4 my-4 [container-type:inline-size] cursor-pointer"
-    >
+    <div onClick={handleCardClick} className="group/card container max-w-[818px] border-b-2 border-black pb-4 my-4 [container-type:inline-size] cursor-pointer">
       <div className="flex justify-between items-center">
         <h4 className="uppercase !text-primary-red">{formattedDate}</h4>
 
@@ -37,7 +54,9 @@ const EventCard = ({ event, onDeleted, onEdit }) => {
       </div>
 
       <div className="flex flex-row-reverse sm:flex-row cq-[min-width:640px]:grid cq-[min-width:640px]:grid-cols-[159px_1fr] gap-4 mt-2 items-start">
-        <figure className="bg-gray-500 w-[100px] h-[100px] shrink-0 cq-[min-width:800px]:w-[200px] cq-[min-width:800px]:h-[200px]"></figure>
+        <figure className="bg-gray-500 w-[100px] h-[100px] shrink-0 cq-[min-width:800px]:w-[200px] cq-[min-width:800px]:h-[200px] overflow-hidden flex items-center justify-center rounded">
+          {artworkImg ? <img src={artworkImg} alt="Event billede" className="object-cover w-full h-full" /> : null}
+        </figure>
 
         <div className="flex flex-col gap-1 w-full">
           <p className="italic font-medium">"{event.title}"</p>
@@ -45,11 +64,7 @@ const EventCard = ({ event, onDeleted, onEdit }) => {
 
           <div className="flex flex-row items-center gap-1">
             <LuMapPin className="text-primary-red" />
-            <p>
-              {event.location?.name && event.location?.address
-                ? `${event.location.name}, ${event.location.address}`
-                : "Lokation ikke tilgængelig"}
-            </p>
+            <p>{event.location?.name && event.location?.address ? `${event.location.name}, ${event.location.address}` : "Lokation ikke tilgængelig"}</p>
           </div>
 
           <div className="flex justify-between gap-4">
@@ -66,10 +81,7 @@ const EventCard = ({ event, onDeleted, onEdit }) => {
           </div>
         </div>
       </div>
-      <div
-        className="flex justify-end shrink-0 items-end gap-4 my-3"
-        onClick={(e) => e.stopPropagation()}
-      >
+      <div className="flex justify-end shrink-0 items-end gap-4 my-3" onClick={(e) => e.stopPropagation()}>
         <SignedIn>
           <DeleteBtn eventId={event.id} onDeleted={onDeleted} />
           <EditIcon event={event} onEdit={onEdit} />
