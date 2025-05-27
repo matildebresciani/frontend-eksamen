49cac31feebdfd55a3e5d725eb35c1395f21bda9
MajaOlsen <majabacholsen@gmail.com>
2025-05-27 09:55:35 +0200

    mindre styling på artcart

--- src/components/single_event/art_gallery/TestArtCart.jsx
+++ src/components/single_event/art_gallery/TestArtCart.jsx
@@ -1,3 +1,4 @@
+//maja
 import { useState, useEffect } from "react";
 import Image from "next/image";
 import Link from "next/link";
@@ -28,26 +29,31 @@ const TestArtCart = ({ artworkId, eventId }) => {
     return <div>Loading...</div>;
   }
 
+  const linkHref = `/events/${eventId}/artwork/${artwork.object_number}`;
+
   return (
-    <div className="relative flex items-center justify-center overflow-hidden rounded-xl shadow-lg bg-white max-w-[340px] max-h-[420px] min-w-[200px] min-h-[200px]">
+    <Link
+      href={linkHref}
+      className="relative flex items-center justify-center overflow-hidden rounded-xl shadow-lg bg-white max-w-[340px] max-h-[420px] min-w-[200px] min-h-[200px] cursor-pointer group"
+    >
       <Image
         src={artwork.image_thumbnail || "/placeholder.jpg"}
         alt={artwork.titles?.[0]?.title || "Artwork"}
         width={600}
         height={800}
-        className="object-contain max-h-[400px] max-w-[340px] w-auto h-auto transition duration-300 hover:opacity-40"
+        className="object-contain max-h-[400px] max-w-[340px] w-auto h-auto transition duration-300 group-hover:opacity-40"
       />
-      <div className="absolute inset-0 flex flex-col justify-end px-6 py-4 opacity-0 transition-opacity duration-300 hover:opacity-100 bg-white/70 backdrop-blur-xs rounded-xl">
-        <span className="text-white font-semibold text-xl drop-shadow mb-2 break-words leading-tight max-w-full text-balance sm:text-md md:text-lg lg:text-xl line-clamp-2 ">
+      <div className="absolute inset-0 flex flex-col justify-end px-6 py-4 opacity-0 transition-opacity duration-300 group-hover:opacity-100 bg-white/10 backdrop-blur-[2px] rounded-xl">
+        <span className="text-white font-semibold text-xl mb-2 break-words leading-tight max-w-full text-balance sm:text-md md:text-lg lg:text-xl line-clamp-2 text-shadow-red">
           {artwork.titles?.[0]?.title || "TITEL PÅ BILLEDE"}
         </span>
-        <Link href={`/events/${eventId}/artwork/${artwork.object_number}`} className="mt-2 group/button">
+        <span className="mt-2 group/button pointer-events-none">
           <BtnWithArrow>
             <span className="font-semibold text-lg">LÆS MERE</span>
           </BtnWithArrow>
-        </Link>
+        </span>
       </div>
-    </div>
+    </Link>
   );
 };
 
