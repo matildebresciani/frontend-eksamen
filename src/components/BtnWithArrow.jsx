3570cd6a637972d17456b3c5ade13f859829d675
matilde <mabr0005@stud.kea.dk>
2025-05-27 09:07:57 +0200

    opdateret btn w arrow

--- src/components/BtnWithArrow.jsx
+++ src/components/BtnWithArrow.jsx
@@ -5,7 +5,7 @@
 const BtnWithArrow = ({ children }) => {
   return (
     <>
-      <div className="text-sm sm:text-md md:text-lg group flex justify-between items-center gap-2 text-primary-red transform">
+      <div className="text-sm sm:text-md md:text-lg group inline-flex items-center gap-2 text-primary-red transform">
         {children}
         <div className="relative h-1 w-6 transition-all duration-300 group-hover:w-10">
           <span className="absolute top-1/2 left-0 h-0.5 w-full bg-primary-red transform -translate-y-1/2" />
