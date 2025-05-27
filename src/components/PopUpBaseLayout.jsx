f232d58b30446ed000b939eabc776cdc8835538a
matilde <mabr0005@stud.kea.dk>
2025-05-27 09:25:15 +0200

    cursor pop up

--- src/components/PopUpBaseLayout.jsx
+++ src/components/PopUpBaseLayout.jsx
@@ -1,6 +1,6 @@
 const PopUpBase = ({children}) => {
     return (         
-        <div className="fixed inset-0 bg-black/50 backdrop-blur-sm flex items-center justify-center z-400">
+        <div className="fixed inset-0 bg-black/50 backdrop-blur-sm flex items-center justify-center z-400 cursor-default">
           <div className="bg-white rounded-lg p-6 shadow-lg min-w-[320px] max-w-[95vw] max-h-[90vh] overflow-auto">
             {children}
           </div>
