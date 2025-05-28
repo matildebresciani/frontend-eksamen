20bd6427b57061f8d92b87d57b14e98dbb3f2807
matilde <mabr0005@stud.kea.dk>
2025-05-28 10:32:19 +0200

    single event fetch fra api mappe

--- src/app/events/[eventId]/page.jsx
+++ src/app/events/[eventId]/page.jsx
@@ -7,20 +7,25 @@ import { useParams } from "next/navigation";
 import TestGallery from "@/components/single_event/art_gallery/TestGallery";
 import Eventinfo from "@/components/single_event/EventInfo";
 import SignUpForm from "@/components/single_event/sign_up/SignUpForm";
+import { fetchEventById } from "@/api-mappe/EventsApiKald";
 
 export default function Eventpage() {
     const { eventId } = useParams();
     const [event, setEvent] = useState(null);
   
     useEffect(() => {
-      const fetchEvent = async () => {
-        const res = await fetch(`https://server-gititgirls.onrender.com/events/${eventId}`);
-        const data = await res.json();
-        setEvent(data);
-      };
-  
-      if (eventId) fetchEvent();
-    }, [eventId]);
+        const getEvent = async () => {
+          try {
+            const data = await fetchEventById(eventId);
+            setEvent(data);
+          } catch (error) {
+            console.error("Kunne ikke hente event:", error);
+          }
+        };
+      
+        if (eventId) getEvent();
+      }, [eventId]);
+      
 
     const handleEdit = (updatedEvent) => {
         console.log("Updated event modtaget fra API:", updatedEvent);
